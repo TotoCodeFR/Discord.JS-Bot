a3482779c4b4126ee0d3ba14861438adeb169956
TotoCode <totocodefr@duck.com>
2025-05-04 16:01:29 +0000

    Ajout des commandes ban, unban et kick + début du panel

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -1,67 +1,86 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageFlags, PermissionsBitField, SlashCommandBuilder } from 'discord.js';
-import path from 'path'
-import { fileURLToPath } from "url";
-import 'dotenv/config';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, MessageFlags, PermissionsBitField, SlashCommandBuilder } from "discord.js";
 
 export default {
     data: new SlashCommandBuilder()
-        .setName('ban')
-        .setDescription('Banne une personne.')
+        .setName("ban")
+        .setDescription("Bannis un utilisateur pour la raison donnée et le temps donné (si un)")
         .addUserOption(option => option
             .setName("utilisateur")
-            .setDescription("L'utilisateur à bannir")
+            .setDescription("L'utilisateur qu'il faut bannir")
             .setRequired(true)
         )
         .addStringOption(option => option
             .setName("raison")
-            .setDescription("La raison pour bannir l'utilisateur.")
+            .setDescription("La raison pourquoi le bannir.")
         ),
+    async execute(interaction) { // L'intéraction est ce qui permet à notre bot de répondre à la commande, où bien de savoir qui l'a exécuté
+        // Un truc cool avec VS Code est que si tu écris quelque chose qui n'a pas été importé, ça te le rajoute
+        // à tes imports.
+        
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral }); // En faisant ça, ça va marquer "Le bot réfléchit" jusqu'à ce
+        // qu'on utilise la fonction interaction.editReply()
+        // MessageFlags.Ephemeral va juste permettre à l'admin de "Rejeter le message" (et il sera aussi visible qu'à lui)
 
-    async execute(interaction) {
-        await interaction.deferReply({ flags: MessageFlags.Ephemeral })
+        // On check si c'est bien quelqu'un qui peut ban
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
             interaction.editReply({ content: '# Mais tu te crois pour qui?\n\nTu n\'a pas la permission pour utiliser cette commande!' })
         }
 
-        const user = interaction.options.getUser("utilisateur")
-
-        const embed = new EmbedBuilder()
-        .setTitle("Bannir <@" + user.id + ">?")
-        .setDescription("<@" + interaction.member.id + "> veut bannir <@" + user.id + ">.")
-
-        const confirm = new ButtonBuilder()
-        .setLabel("Oui, je veux bannir cette personne.")
-        .setEmoji('✅')
-        .setStyle(ButtonStyle.Success)
-        .setCustomId("confirm")
-
-        const cancel = new ButtonBuilder()
-        .setLabel("Non, je change d'idée.")
-        .setEmoji('❌')
-        .setStyle(ButtonStyle.Danger)
-        .setCustomId("cancel")
-
-        const row = new ActionRowBuilder()
-        .addComponents(confirm, cancel)
-
-        interaction.editReply({ embeds: [embed], components: [row], withResponse: true })
+        const utilisateurABannir = interaction.options.getUser("utilisateur");
+        const confirmerEmbed = new EmbedBuilder()
+            .setColor("#ff6e6e") // un rouge clair // allez sur google colors
+            .setTitle("Bannir " + utilisateurABannir.username) // titre + pseudo de la personne à bannir
+            .addFields(
+                { name: 'Confirmer le ban de ' + utilisateurABannir.username,
+                    value: 'Êtes-vous sûr de vouloir bannir ' + utilisateurABannir.username }
+            )
+        
+        // Bouton confirmer
+        const confirmer = new ButtonBuilder()
+            .setCustomId("confirmerBan") // IMPORTANT - Permet de de déterminer le quel bouton est le quel (on ne peut pas utiliser les variables)
+            .setLabel("Oui, je veux le bannir") // Le texte sur le bouton
+            .setEmoji("✅") // L'emoji sur le bouton
+            .setStyle(ButtonStyle.Success) // Sa couleur de fond (
+        
+        const refuser = new ButtonBuilder()
+            .setCustomId("refuserBan")
+            .setLabel("Non, je change d'idée")
+            .setEmoji("❌")
+            .setStyle(ButtonStyle.Danger)
+        
+        // Pour créer des boutons, il faut une ligne puis les boutons
+        const ligne = new ActionRowBuilder()
+            .addComponents(confirmer, refuser) // On rajoute les boutons à la ligne
+        
+        const reponse = await interaction.editReply({ 
+            embeds: [confirmerEmbed],
+            components: [ligne]
+        });
 
         const collectorFilter = i => i.user.id === interaction.user.id;
 
         try {
-            const confirmation = await response.resource.message.awaitMessageComponent({ filter: collectorFilter, time: 120_000 });
+            const confirmation = await reponse.awaitMessageComponent({ 
+                filter: collectorFilter, 
+                time: 120_000 
+            });
 
-            if (confirmation.customId === "confirm") {
-                await interaction.guild.members.ban(user)
-                await confirmation.update({ content: 'Utilisateur banni.' })
-            } else if (confirmation.customId === "cancel") {
-                await confirmation.update({ content: 'Action annulée.', components: [] })
+            if (confirmation.customId === "confirmerBan") {
+                await interaction.guild.members.ban(utilisateurABannir);
+                await confirmation.update({ 
+                    content: `${utilisateurABannir.username} banni.`, 
+                    components: [], 
+                    embeds: [] 
+                });
+            } else if (confirmation.customId === "refuserBan") {
+                await confirmation.update({ 
+                    content: 'Action annulée.', 
+                    components: [], 
+                    embeds: [] 
+                });
             }
-        } catch {
-            await interaction.editReply({ content: 'Aucune confirmation reçue!', components: [] });
+        } catch (e) {
+            await interaction.editReply({ content: 'Aucune confirmation reçue!', components: [], embeds: [] });
         }
-    },
-};
+    }
+}
